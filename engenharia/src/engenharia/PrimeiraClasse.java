35166c6bbc947c0d8e105f56cfc867b7ecd52606
Enzo Fuji <enzo.juniti@gmail.com>
2020-05-25 17:46:43 -0300

    Projetinho Java

---
+++ engenharia/src/engenharia/PrimeiraClasse.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package engenharia;
+
+/**
+ * @author enzoj
+ *
+ */
+public class PrimeiraClasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}

15c9d5f7fc6da234b8f3b887cbdca5515d7cd90d
Enzo Fuji <enzo.juniti@gmail.com>
2020-05-26 19:55:35 -0300

    Create ajuda.py
    arquivo de ajuda

---
+++ ajuda.py
@@ -0,0 +1,8 @@
+"""
+Caso você encontre dificuldade de abertura do arquivo, 
+esse programa serve apenas para te ajudar.
+
+Vá na pasta Engenharia e entre na subpasta  "src".
+Ali dentro, você encontrará o arquivo executável em Java.
+
+"""
